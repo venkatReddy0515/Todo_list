d6d98c8351b4e610536ae0a347f294a8245d7fc2
Jonnagiri venkatreddy <22x51a0548@srecnandyal.edu.in>
2024-12-15 18:40:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors({origin: 'https://todolistmern-23vt.onrender.com/'}));
 app.use(express.json());
 dotenv.config();
 
