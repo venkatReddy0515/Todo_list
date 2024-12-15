e93e2417e79d66b2035609678d68927048dc75b0
Jonnagiri venkatreddy <22x51a0548@srecnandyal.edu.in>
2024-12-15 18:33:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,13 +11,10 @@ dotenv.config();
 
 const port = process.env.PORT || 4000;
 
-mongoose.connect(process.env.MONGO_URL)
-    .then(() => {
-        console.log("Connection is Done.");
-    })
-    .catch((err) => {
-        console.log("Connection failed::", err);
-    });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('Database connected!'))
+.catch(err => console.error('Database connection error:', err));
+
 
 app.get("/", async (req, res) => {
     try {
